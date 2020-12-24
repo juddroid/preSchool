a32c0dfbfaef1e563e0e7151da36d2d010c4b3c1
Raccoon <nobllizdc@gmail.com>
2020-12-24 16:51:07 +0900

    1에서 무한대까지 인자를 받아 합을 구하는 함수

--- javascriptFn.js
+++ javascriptFn.js
@@ -1,4 +1,11 @@
-function a() {
-  console.log(arguments);
+// 해볼만한 것(1~무한대까지 인자를 받아 합을 구하는 함수를 만들어보자)
+
+function infinitySum() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum;
 }
-a(1, 2, 3);
+
+console.log(infinitySum(3, 5, 6))
