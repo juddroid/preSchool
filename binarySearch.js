80285860d97e1f22b194f85252e35424733b0429
Raccoon <nobllizdc@gmail.com>
2020-12-29 21:37:56 +0900

    BinarySearsh

---
+++ binarySearch.js
@@ -0,0 +1,35 @@
+let searchCount = 0;
+
+function getNumArr(n) {
+  let numArr = [];
+  for (let i = 1; i < n + 1; i++) {
+    numArr.push(i);
+  }
+  return numArr;
+}
+
+function binarySearch(arr, num) {
+  searchCount++;
+  if (arr[parseInt(arr.length / 2)] === num) {
+    return console.log(`Find: ${num}, Count: ${searchCount}`);
+  }
+  if (arr[parseInt(arr.length / 2)] > num) {
+    arr.splice(parseInt(arr.length / 2), arr.length - parseInt(arr.length / 2));
+    binarySearch(arr, num);
+  }
+  if (arr[parseInt(arr.length / 2)] < num) {
+    arr.splice(0, arr.length - parseInt(arr.length / 2));
+    binarySearch(arr, num);
+  }
+}
+
+function test(testNum) {
+  for (let i = 1; i < testNum + 1; i++) {
+    binarySearch(getNumArr(testNum), i);
+    searchCount = 0;
+  }
+}
+
+test(100);
+// binarySearch(getNumArr(100), 2);
+// binarySearch(getNumArr(100), 4);
