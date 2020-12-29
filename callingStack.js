93ff8c833e86c528e1960ae7b085d83a0d392a2b
Raccoon <nobllizdc@gmail.com>
2020-12-29 14:45:05 +0900

    printExecutionSequence

--- callingStack.js
+++ callingStack.js
@@ -1,32 +1,52 @@
+const shapeSet = {
+  circle: 'circle',
+  rect: 'rect',
+  trapezoid: 'trapezoid'
+}
+const logList = [];
+
 const getCircleArea = (r, n = r) => {
+  logList.push(shapeSet.circle)
   let circleArea = Math.PI * r ** 2
   if (r === n) {
     return circleArea
   }
   return circleArea + getCircleArea(r + 1, n)
 }
-const getRectArea = (rows, cols) => rows * cols;
-const getTrapezoidArea = (top, bottom, height) => ((top + bottom) / 2) * height;
+const getRectArea = (rows, cols) => {
+  logList.push(shapeSet.rect)
+  return rows * cols;
+}
+const getTrapezoidArea = (top, bottom, height) => {
+  logList.push(shapeSet.trapezoid)
+  return ((top + bottom) / 2) * height;
+}
 
 function getArea(shape, ...length) {
-  if (shape === 'circle') {
+  if (shape === shapeSet.circle) {
     return getCircleArea(length[0], length[1])
   }
-  if (shape === 'rect') {
+  if (shape === shapeSet.rect) {
     return getRectArea(length[0], length[1])
   }
-  if (shape === 'trapezoid') {
+  if (shape === shapeSet.trapezoid) {
     return getTrapezoidArea(length[0], length[1], length[2])
   }
 }
-
-
-// console.log(getArea('circle', 4))
-
-
 
 
 console.log(`circle: ${getArea('circle', 3)}`)
 console.log(`rect: ${getArea('rect', 3, 5)}`)
 console.log(`trapezoid: ${getArea('trapezoid', 3, 5, 7)}`)
 console.log(`circleSum: ${getArea('circle', 3, 5)}`)
+
+
+function printExecutionSequence() {
+  let printExecution = '';
+  for (let i = 0; i < logList.length; i++) {
+    printExecution += logList[i] + ', ';
+  }
+  return console.log(`Execution Sequence >> ${printExecution}`)
+}
+
+printExecutionSequence()
