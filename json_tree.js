b8d48ee17e0fba4e2fec7191e97af4d7a8bf155d
Raccoon <nobllizdc@gmail.com>
2020-12-31 18:01:11 +0900

    json_tree

---
+++ json_tree.js
@@ -0,0 +1,119 @@
+const sample = [
+  {
+    id: 1,
+    name: 'Yong',
+    phone: '010-0000-0000',
+    type: 'sk',
+    childnode: [
+      {
+        id: 11,
+        name: 'echo',
+        phone: '010-0000-1111',
+        type: 'kt',
+        childnode: [
+          {
+            id: 115,
+            name: 'hary',
+            phone: '211-1111-0000',
+            type: 'sk',
+            childnode: [
+              {
+                id: 1159,
+                name: 'pobi',
+                phone: '010-444-000',
+                type: 'kt',
+                childnode: [
+                  {
+                    id: 11592,
+                    name: 'cherry',
+                    phone: '111-222-0000',
+                    type: 'lg',
+                    childnode: [],
+                  },
+                  {
+                    id: 11595,
+                    name: 'solvin',
+                    phone: '010-000-3333',
+                    type: 'sk',
+                    childnode: [],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 116,
+            name: 'kim',
+            phone: '444-111-0200',
+            type: 'kt',
+            childnode: [
+              {
+                id: 1168,
+                name: 'hani',
+                phone: '010-222-0000',
+                type: 'sk',
+                childnode: [
+                  {
+                    id: 11689,
+                    name: 'ho',
+                    phone: '010-000-0000',
+                    type: 'kt',
+                    childnode: [
+                      {
+                        id: 116890,
+                        name: 'wonsuk',
+                        phone: '010-000-0000',
+                        type: 'kt',
+                        childnode: [],
+                      },
+                      {
+                        id: 1168901,
+                        name: 'chulsu',
+                        phone: '010-0000-0000',
+                        type: 'sk',
+                        childnode: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 117,
+            name: 'hong',
+            phone: '010-0000-0000',
+            type: 'lg',
+            childnode: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+let arr = [];
+function pushName(name) {
+  arr.push(name);
+}
+
+function checkSK(obj) {
+  if (obj.type === 'sk') {
+    pushName(obj.name);
+  }
+}
+
+function checkTypeOf(data) {
+  if (typeof data === 'object' && Array.isArray(data)) {
+    data.forEach((element) => {
+      checkTypeOf(element);
+    });
+  }
+  if (typeof data === 'object' && !Array.isArray(data)) {
+    checkSK(data);
+    checkTypeOf(Object.values(data));
+  }
+  return arr;
+}
+
+console.log(checkTypeOf(sample));
