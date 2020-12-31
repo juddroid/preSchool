cf48d2e7f5bf2cd242d4f3920cc79ef61d492cab
Raccoon <nobllizdc@gmail.com>
2020-12-31 21:46:52 +0900

    pracReduce

--- pracReduce.js
+++ pracReduce.js
@@ -1,7 +1,57 @@
 const array1 = [1, 2, 3, 4];
 
-const myReduce = (arr, callback, initialValue) => {
-  //여기에 구현
+const myReduce = (arr, callback, ...initial) => {
+  // initialValueCheck
+  let initialValueCheck = initial[0] === undefined ? true : false;
+  // setting initialValue
+  let initialValue;
+  if (arr.length === 0 && initialValueCheck) {
+    console.error('Uncaught TypeError: Reduce of empty array with no initial value');
+  }
+  if (initialValueCheck) {
+    initialValue = arr[0];
+  }
+  initialValue = initial[0];
+
+  // callbackFn
+  callback = function callbackFn(accumulator, currentValue, ...rest) {
+    // setting accumulator
+    // setting currentValue
+    if (initialValueCheck) {
+      accumulator = initialValue;
+      currentValue = arr[0];
+    }
+    currentValue = initialValue;
+
+    // setting currentIndex
+    let currentIndex;
+    rest[0] = currentIndex;
+    if (initialValueCheck) {
+      currentIndex = 0;
+    }
+    currentIndex = 1;
+    // readonly
+    rest[1] = arr;
+    return accumulator;
+  };
+  // reducer
+  for (element in arr) {
+    // except empty property in array
+    if (arr[element] === undefined) {
+      return;
+    }
+    callback;
+  }
+
+  return;
 };
 
-const result = myReduce(arr, (next, prev) => {}, []);
+const result = myReduce(
+  array1,
+  (next, prev) => {
+    next + prev;
+  },
+  []
+);
+
+console.log(result);
