51c88993b46f63677a313a44ad7701dcf8fd052b
Raccoon <nobllizdc@gmail.com>
2021-01-01 13:23:33 +0900

    reduce

--- pracReduce.js
+++ pracReduce.js
@@ -1,57 +1,55 @@
 const array1 = [1, 2, 3, 4];
+const array2 = [];
+const callback = (next, prev) => next + prev;
 
-const myReduce = (arr, callback, ...initial) => {
+const myReduce = (arr, callbackFn, ...initial) => {
   // initialValueCheck
-  let initialValueCheck = initial[0] === undefined ? true : false;
+  let initialValueCheck = initial[0] !== undefined ? true : false;
   // setting initialValue
-  let initialValue;
   if (arr.length === 0 && initialValueCheck) {
-    console.error('Uncaught TypeError: Reduce of empty array with no initial value');
+    return console.error('Uncaught TypeError: Reduce of empty array with no initial value');
   }
-  if (initialValueCheck) {
-    initialValue = arr[0];
-  }
-  initialValue = initial[0];
-
-  // callbackFn
-  callback = function callbackFn(accumulator, currentValue, ...rest) {
-    // setting accumulator
-    // setting currentValue
-    if (initialValueCheck) {
-      accumulator = initialValue;
-      currentValue = arr[0];
-    }
-    currentValue = initialValue;
-
-    // setting currentIndex
-    let currentIndex;
-    rest[0] = currentIndex;
-    if (initialValueCheck) {
-      currentIndex = 0;
-    }
+  let accumulator;
+  let currentValue;
+  let currentIndex;
+  if (!initialValueCheck) {
+    accumulator = arr[0];
+    currentValue = arr[1];
     currentIndex = 1;
-    // readonly
-    rest[1] = arr;
-    return accumulator;
-  };
-  // reducer
-  for (element in arr) {
-    // except empty property in array
-    if (arr[element] === undefined) {
-      return;
-    }
-    callback;
   }
 
-  return;
+  if (initialValueCheck) {
+    accumulator = initial[0];
+    currentValue = arr[0];
+    currentIndex = 0;
+  }
+  // iterator
+  arr.forEach((currentValue) => {
+    // except empty property in array
+    // if (arr[currentValue] === undefined) {
+    //   return;
+    // }
+    // callbackFn: reducer
+    accumulator = callbackFn(accumulator, currentValue);
+  });
+  return accumulator;
 };
 
-const result = myReduce(
-  array1,
-  (next, prev) => {
-    next + prev;
-  },
-  []
-);
+const result = myReduce(array1, callback);
 
 console.log(result);
+
+const arr = [1, 2, 3, 4, 5];
+const add = (a, b) => a + b;
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+console.log(reduce(add, 0, arr));
