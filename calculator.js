46b4480711abd474343375bb3a473f6c42fbb11a
Juddroid <nobllizdc@gmail.com>
2021-01-04 00:58:19 +0900

    Fix caculator

--- calculator.js
+++ calculator.js
@@ -3,19 +3,18 @@ function calculate(cmd, num1, num2) {
     case 'plus':
       return num1 + num2
     case 'minus':
-      return num1 + num2
+      return num1 - num2
     case 'multiple':
       return num1 * num2;
     case 'divider':
       return num1 / num2;
     default:
-      throw Error('error');
+      throw Error(cmd);
   }
 }
-
 
 console.log(calculate('plus', 1, 2))
 console.log(calculate('minus', 1, 2))
 console.log(calculate('multiple', 1, 2))
-console.log(calculate('divider', 1, 2))
-// console.log(calculate('d', 1, 2))
+calculate('divider', 1, 2)
+calculate('d', 1, 2)
